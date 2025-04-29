59c6318c48f725f271935705749b1961c4a7e545
Babita <babita0312@gmail.com>
2025-04-29 12:03:42 +0530

    add

--- aws_instance_ec2/main.tf
+++ aws_instance_ec2/main.tf
@@ -5,15 +5,19 @@ resource "aws_instance" "demo_tf" {
   key_name                    = "tf-demo"
   vpc_security_group_ids      = [aws_security_group.main.id]
 
+  depends_on = [aws_security_group.main] # ensure SG is ready
+
   tags = {
     Name = "AnsibleMaster"
   }
 
   connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("C:/Users/Admin/Downloads/demo/tf-demo.pem")
-    host        = self.public_ip
+    type         = "ssh"
+    user         = "ec2-user"
+    private_key  = file("C:/Users/Admin/Downloads/demo/tf-demo.pem")
+    host         = self.public_ip
+    timeout      = "2m" # wait for SSH to come up
+    bastion_host = null
   }
 
   provisioner "file" {
@@ -22,11 +26,16 @@ resource "aws_instance" "demo_tf" {
 slave ansible_host=${var.slave_ip} ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/ansible_master.pem
 EOF
     destination = "/home/ec2-user/inventory"
+
+    on_failure = continue
   }
 
   provisioner "file" {
     source      = "C:/Users/Admin/Downloads/demo/ansible_master.pem"
     destination = "/home/ec2-user/ansible_master.pem"
+
+    on_failure = continue
+
   }
 
   provisioner "file" {
@@ -35,33 +44,36 @@ EOF
 - name: Ping Slave
   hosts: slaves
   tasks:
-    - name: Ping
-      ping:
+    - name: upgrade packages
+      ansible.builtin.yum:
+        name: nginx       
+        state: present
+        update_cache: yes
 EOF
     destination = "/home/ec2-user/ping.yaml"
+
+    on_failure = continue
   }
 
   provisioner "remote-exec" {
     inline = [
+      "sleep 30", # Wait for EC2 readiness (good practice)
       "sudo yum update -y",
-      "sudo dnf install -y ansible", #If you want to keep using Amazon Linux 2023, you'll have to install Ansible via dnf:
-      # "amazon-linux-extras install ansible2 -y",  # AMI ID with Amazon Linux 2
+      "sudo dnf install -y python3-pip",
+      "pip3 install ansible",
       "chmod 400 /home/ec2-user/ansible_master.pem",
+      "ansible --version",
       "ansible-playbook -i /home/ec2-user/inventory /home/ec2-user/ping.yaml"
     ]
 
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("C:/Users/Admin/Downloads/demo/tf-demo.pem")
-      host        = self.public_ip
-    }
+    on_failure = continue
+
   }
 }
 
 variable "slave_ip" {
   type        = string
-  description = "Private or Public IP of manually created slave EC2 instance 13.203.219.181"
+  description = "Private or Public IP of manually created slave EC2 instance 43.204.229.105"
 }
 
 
